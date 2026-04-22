7d8cd90924c44f83f6ec77891ea3fc5ceb74a5a6
bnradon <brandonguerreroj@gmail.com>
2026-04-22 16:04:09 -0500

    node.js fetch settings

--- src/controllers/items.controllers.js
+++ src/controllers/items.controllers.js
@@ -13,8 +13,6 @@ exports.getItemById = async (req, res) => {
   res.json({ id: doc.id, ...doc.data() });
 };
 
-const fetch = require("node-fetch");
-
 exports.createItem = async (req, res) => {
   const { name, price } = req.body;
 
@@ -45,6 +43,7 @@ exports.updateItem = async (req, res) => {
   await collection.doc(req.params.id).update({ name, price: Number(price) });
   res.json({ id: req.params.id, name, price: Number(price) });
 };
+
 
 exports.deleteItem = async (req, res) => {
   await collection.doc(req.params.id).delete();
